6db04c4caf23c0cdf7277635e92e427dc1273028
Heljä Lehtinen <lehthelj@gmail.com>
2024-02-02 21:43:48 +0200

    Fixing add new game page

--- games.py
+++ games.py
@@ -3,32 +3,28 @@ from db import db
 def add_new_game(player1, player2, elo1, elo2, event, date):
     player1_id = db.session.execute("""
     SELECT IFNULL(id, 0)
-    FROM Players WHERE name=:name""", player1)
+    FROM Players WHERE name=:name""", player1).fetchone()[0]
 
     player2_id = db.session.execute("""
     SELECT IFNULL(id, 0)
-    FROM Players WHERE name=:name""", player2)
+    FROM Players WHERE name=:name""", player2).fetchone()[0]
 
     if player1_id == 0:
         sql1 = "INSERT INTO Players (name, elo) VALUES (:player1, :elo1)"
         db.session.execute(sql1, {"name":player1, "elo":elo1})
         player1_id = db.session.execute("""SELECT * FROM Players
-                                       ORDER BY id DESC LIMIT 1""")
+                                       ORDER BY id DESC LIMIT 1""").fetchone()[0]
     if player2_id == 0:
         sql1 = "INSERT INTO Players (name, elo) VALUES (:player1, :elo1)"
-        db.session.execute(sql1, {"name":player1, "elo":elo1})
+        db.session.execute(sql1, {"name":player2, "elo":elo2})
         player2_id = db.session.execute("""SELECT * FROM Players
-                                       ORDER BY id DESC LIMIT 1""")
+                                       ORDER BY id DESC LIMIT 1""").fetchone()[0]
 
     sql = """INSERT INTO Games 
     (player1, player2, elo1, elo2, event, date)
-    VALUES (:player1, :player2, :elo1, :elo2, :event, :date)
-    RETURNING id"""
-    db.session.execute(sql, {"player1":player1_id, "player":player2_id,
+    VALUES (:player1, :player2, :elo1, :elo2, :event, :date)"""
+    db.session.execute(sql, {"player1":player1_id, "player2":player2_id,
                             "elo1":elo1, "elo2":elo2,
                             "event":event, "date":date})
     db.session.commit()
-    return True
 
-# TODO:
-    # adding players separately into db
--- routes.py
+++ routes.py
@@ -6,15 +6,17 @@ import games
 def index():
     return render_template("index.html")
 
-@app.route("/new", methods=["POST"])
+@app.route("/new")
 def new():
+    return render_template("new.html")
+
+@app.route("/add", methods=["POST"])
+def add():
     player1 = request.form["player1"]
     player2 = request.form["player2"]
     elo1 = request.form["elo1"]  # elo (rating) during game
     elo2 = request.form["elo2"]
     event = request.form["event"]
     date = request.form["date"]
-    game = games.add_new_game(player1, player2, elo1, elo2, event, date)
-    if not game:
-        pass   # todo
-    return redirect("/")
+    games.add_new_game(player1, player2, int(elo1), int(elo2), event, date)
+    return render_template("/")

498604f18fa3a9d71161f71d0e4a1532a6576cf3
Heljä Lehtinen <lehthelj@gmail.com>
2024-02-08 16:45:34 +0200

    user can print all games

--- routes.py
+++ routes.py
@@ -20,3 +20,8 @@ def add():
     date = request.form["date"]
     games.add_new_game(player1, player2, int(elo1), int(elo2), event, date)
     return redirect("/")
+
+@app.route("/print")
+def print():
+    all = games.get_all()
+    return render_template("print.html", items=all)
